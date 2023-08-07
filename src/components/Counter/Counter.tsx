931727701e12cfcd109ed8c56a3eaecfb45b5fe9
Aliaksandr91 <horbachalex91@gmail.com>
2023-08-07 15:31:21 +0200

    feat: little refactoring

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -16,33 +16,40 @@ export const Counter = (props: PropsType) => {
     const incDisabled: boolean = props.count >= props.maxValue
     const resetDisabled: boolean = props.count === props.initialValue
 
+    const counterScreen = props.screenStatus === 'counter'
+    const settingsScreen = props.screenStatus === 'settings'
+
+
     return (
-
-
         <div className='counter'>
             {
-                (() => {
-                    switch (props.screenStatus) {
-                        case 'counter':
-                            return <div className={incDisabled ? 'counter__screen counter__screen--max' : 'counter__screen'}>{props.count}</div>
-                        case 'settings':
-                            return <div className='counter__screen counter__screen--text'>enter values and press 'set'</div>
-                        case 'error':
-                            return <div className='counter__screen counter__screen--error'>Incorrect value!</div>
-                    }
-                })()
+                counterScreen
+                    ? <div className={incDisabled ? 'counter__screen counter__screen--max' : 'counter__screen'}>{props.count}</div>
+                    : settingsScreen
+                        ? <div className='counter__screen counter__screen--text'>enter values and press 'set'</div>
+                        : <div className='counter__screen counter__screen--error'>Incorrect value!</div>
+                // (() => {
+                //     switch (props.screenStatus) {
+                //         case 'counter':
+                //             return <div className={incDisabled ? 'counter__screen counter__screen--max' : 'counter__screen'}>{props.count}</div>
+                //         case 'settings':
+                //             return <div className='counter__screen counter__screen--text'>enter values and press 'set'</div>
+                //         case 'error':
+                //             return <div className='counter__screen counter__screen--error'>Incorrect value!</div>
+                //     }
+                // })()
             }
 
             <div className='counter__controls'>
                 <Button
                     name={"inc"}
                     callBack={props.onIncrementCount}
-                    disabledStatus={props.screenStatus === 'counter' ? incDisabled : true}
+                    disabledStatus={counterScreen ? incDisabled : !counterScreen}
                 />
                 <Button
                     name={'reset'}
                     callBack={props.onResetCount}
-                    disabledStatus={props.screenStatus === 'counter' ? resetDisabled : true}
+                    disabledStatus={counterScreen ? resetDisabled : !counterScreen}
                 />
             </div>
         </div>
